27d6454e677e45c5c741395a5a84b2933524282a
chaya2z <chaya2zdev@gmail.com>
2022-07-12 11:43:19 +0900

    CVS-80 型定義の補足

--- host/internal/memory/type.go
+++ host/internal/memory/type.go
@@ -1,8 +1,8 @@
 package memory
 
 type Memory struct {
-	Total       uint64
-	Used        uint64
-	Free        uint64
-	UsedPercent float64
+	Total       uint64  `json:"total" description:"メモリサイズ"`
+	Used        uint64  `json:"used" description:"使用済みサイズ"`
+	Free        uint64  `json:"free" description:"空きサイズ"`
+	UsedPercent float64 `json:"used_percent" description:"使用率 (％)"`
 }
