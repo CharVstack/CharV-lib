6c41b23eef37585ce6ff6c1dfe740f083ad6d848
bluekey <kameyamatakeru12@gmail.com>
2022-09-27 02:36:48 +0900

    Fix サイズとファイルタイプ固定の新しいドライブを作成する関数の追加

--- qemu/install.go
+++ qemu/install.go
@@ -5,8 +5,22 @@ import (
 	"text/template"
 )
 
+func CreateDisk(opts string) error {
+	tmpl, err := template.New("create").Parse(`qemu-img create -f qcow2 /var/lib/charVstack/images/{{.}}.qcow2 8G`)
+	if err != nil {
+		return err
+	}
+	var buf bytes.Buffer
+	if err := tmpl.Execute(&buf, opts); err != nil {
+		return err
+	}
+	cmd := buf.String()
+
+	return run(cmd)
+}
+
 func Install(opts InstallOpts) error {
-	tmpl, err := template.New("install").Parse(`qemu-system-x86_64 -smp {{.VCpu}} -m {{.Memory}} -cdrom "{{.Name}}" -boot order=d -drive file=/var/lib/libvirt/images/test01.qcow2,format=qcow2`)
+	tmpl, err := template.New("install").Parse(`qemu-system-x86_64 -smp {{.VCpu}} -m {{.Memory}} -cdrom "{{.Name}}" -boot order=d -drive file=/var/lib/charVstack/images/{{.Disk}}.qcow2,format=qcow2`)
 	if err != nil {
 		return err
 	}
