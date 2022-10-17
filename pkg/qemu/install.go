5e81059db46bef653f8e87f4d66c0b27e25603a1
bluekey <kameyamatakeru12@gmail.com>
2022-10-17 11:35:54 +0900

    WIP 作業中

--- pkg/qemu/install.go
+++ pkg/qemu/install.go
@@ -2,6 +2,7 @@ package qemu
 
 import (
 	"bytes"
+	"fmt"
 	"text/template"
 )
 
@@ -17,6 +18,37 @@ func CreateDisk(opts string) error {
 	cmd := buf.String()
 
 	return run(cmd)
+}
+
+func GetDiskInfo() {
+
+}
+
+func CreateInfoJSON(opts Vm) Vm {
+	//uuidの作成
+	var createUUID string
+	createUUID = "this-is-uuid"
+
+	//structに合わせる
+	createJSON := Vm{
+		Devices: Devices{},
+		Memory:  opts.Memory,
+		Metadata: Metadata{
+			ApiVersion: "v1",
+			Id:         createUUID,
+		},
+		Name: opts.Name,
+		Vcpu: opts.Vcpu,
+	}
+
+	//Device.Diskの個数を把握し、その分だけstructに代入する
+	for i := 0; i < len(opts.Devices.Disk); i++ {
+		createJSON.Devices.Disk[i].Type = opts.Devices.Disk[i].Type
+		createJSON.Devices.Disk[i].Type = opts.Devices.Disk[i].Path
+		fmt.Println(createJSON.Devices.Disk)
+	}
+
+	return createJSON
 }
 
 func Install(opts InstallOpts) error {
