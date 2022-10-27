4f0163b16f9e32b9b53328fa2c71d3f9a7f3db22
bluekey <kameyamatakeru12@gmail.com>
2022-10-27 12:58:00 +0900

    Fix JSONファイルから型に変換する機能を作成

--- pkg/qemu/list.go
+++ pkg/qemu/list.go
@@ -1,9 +1,14 @@
 package qemu
 
 import (
+	"encoding/json"
+	"fmt"
+	"os"
 	"os/exec"
 	"regexp"
 	"strings"
+
+	"github.com/CharVstack/CharV-lib/domain/models"
 )
 
 func GetRunningList() ([]string, error) {
@@ -22,4 +27,47 @@ func GetRunningList() ([]string, error) {
 		}
 	}
 	return guests, nil
+}
+
+func ConvertJSONToStruct(directoryPath string) ([]models.Vm, error) {
+	var resJSONList []models.Vm
+	files, err := os.ReadDir(directoryPath)
+	if err != nil {
+		return []models.Vm{}, err
+	}
+
+	var openFile *os.File
+	var read = make([]byte, 1024)
+	var cont int
+	var resJSON models.Vm
+
+	for _, file := range files {
+		openFile, err = os.Open(directoryPath + file.Name())
+		if err != nil {
+			fmt.Println(err)
+			return []models.Vm{}, err
+		}
+
+		cont, err = openFile.Read(read)
+		if err != nil {
+			return []models.Vm{}, err
+		}
+
+		err = json.Unmarshal(read[:cont], &resJSON)
+		if err != nil {
+			return []models.Vm{}, err
+		}
+
+		resJSONList = append(resJSONList, resJSON)
+
+	}
+
+	defer func() {
+		err = openFile.Close()
+		if err != nil {
+			fmt.Println("File Close Err: ", err)
+		}
+	}()
+
+	return resJSONList, err
 }
